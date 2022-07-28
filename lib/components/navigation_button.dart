f6b594fbe1344db7a50f2c245394242fbd4e2d51
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-07-28 11:24:19 +0200

    docs(components): update dart doc and add code example

--- lib/components/navigation_button.dart
+++ lib/components/navigation_button.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
+/// Build a a simple button that navigates towards a new page.
+///
+/// The Button uses the current [context] and when clicked executes the [navigationFunction].
+/// The label shown on the button is given via [buttonText].
+/// This way a simple button can be created by simply calling this function, making the code more readable.
+///
+/// Example:
+/// ```dart
+///void navigateToX(BuildContext context)
+///{
+///   Navigator.push(
+///    context,
+///    MaterialPageRoute(
+///      builder: (context) => const StatisticsPage(_statisticsText),
+///    ),
+///  );
+///}
+///
+///const String text = "Text";
+///
+///@override
+/// Widget build(BuildContext context) {
+///   return Scaffold(
+///     appBar: AppBar(title: text),
+///     body:
+///         buildNavigationButton(
+///           context,
+///           navigateToX
+///           text,
+///         )
+///     ),
+///   );
+/// }
+///
+/// ```
 ElevatedButton getNavigationButton(
   BuildContext context,
   Function(BuildContext context) navigationFunction,
