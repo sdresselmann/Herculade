2862a5e6afac1fc54ce3d0829355462f76c3eeb2
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-07-28 10:30:04 +0200

    refactor(home): change privacy to library only for home class attributes and functions
    This way they can't be called from outside their own library.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,37 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:lifting_progress_tracker/pages/statistics.dart';
 
 /// The text used for the button label and the title of the statistics page.
-const String statisticsText = "Statistik";
+const String _statisticsText = "Statistik";
 
-/// The page used as main entry point for the app.
+/// The page used as main entry point for this app.
 class HomePage extends StatelessWidget {
   /// Title of the page as shown in the appbar.
-  final String title;
-  const HomePage(this.title);
+  final String _title;
+
+  const HomePage(this._title);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text(_title)),
       body: Row(
-        children: [buildStatisticsRouteButton(context)],
+        children: [_buildStatisticsRouteButton(context)],
       ),
     );
   }
 }
 
 /// Build a button that when clicked navigates the user towards the statistics page.
-ElevatedButton buildStatisticsRouteButton(BuildContext context) {
+ElevatedButton _buildStatisticsRouteButton(BuildContext context) {
   final ElevatedButton statisticsButton = ElevatedButton(
     onPressed: () {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => const StatisticsPage(statisticsText),
+          builder: (context) => const StatisticsPage(_statisticsText),
         ),
       );
     },
-    child: const Text(statisticsText),
+    child: const Text(_statisticsText),
   );
 
   return statisticsButton;
