48d140342290c256b14357d9cfa71537a99c03d8
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-07-29 20:24:53 +0200

    feat(navigation): add route strings for named routes

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,11 +1,23 @@
-/// Named string for the home route.
+/// Named string for the home page route.
 const String homeRoute = '/';
 
-/// Named string for the statistics route.
+/// Named string for the statistics page route.
 const String statisticsRoute = 'Statistik';
 
-/// Named string for the calendar route.
+/// Named string for the calendar page route.
 const String calendarRoute = "Kalender";
 
-/// Named string for the trainings plan list route.
+/// Named string for the trainings plan list page route.
 const String trainingplanListRoute = "Trainingspläne";
+
+/// Named string for the exercise page route.
+const String exerciseRoute = "Übung";
+
+/// Named string for the training day page route.
+const String trainingDayRoute = "Trainingstag";
+
+/// Named string for the training period page route.
+const String trainingPeriodRoute = "Zeitraum";
+
+/// Named string for the training plan page route.
+const String trainingPlanRoute = "Trainingsplan";
