ed289d4d0d46cfc84d6c81c072f0d0b059cf6560
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-07-29 20:41:18 +0200

    docs(navigation): adjust docs for navigation button builder function
    The signature changed due to the fact that the callback function stays the same and does not need to be handled by a parameter.

--- lib/components/navigation_button.dart
+++ lib/components/navigation_button.dart
@@ -2,15 +2,12 @@ import 'package:flutter/material.dart';
 
 /// Build a a simple button that navigates towards a new page.
 ///
-/// The Button uses the current [context] and when clicked executes the [navigationFunction].
-/// The label shown on the button is given via [buttonText] which is also used as name for the route that is used for navigation.
+/// The Button uses the current [context] and when clicked executes [_navigateToRoute] with the given [routeName].
+/// The label shown on the button is also used as name for the route that is used for navigation.
 /// This way a simple button can be created by simply calling this function, making the code more readable.
 ///
 /// Example:
 /// ```dart
-/// void _navigateToRoute(BuildContext context, String routeName) {
-///   Navigator.pushNamed(context, routeName);
-/// }
 ///
 ///const String targetPage = 'Seite 2';
 ///
@@ -21,7 +18,6 @@ import 'package:flutter/material.dart';
 ///     body:
 ///         buildNavigationButton(
 ///           context,
-///           _navigateToRoute
 ///           targetPage,
 ///         )
 ///     ),
