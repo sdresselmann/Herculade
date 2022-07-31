849e81a99a082b3043f9f6edc69fec953fba1f34
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-07-31 18:08:16 +0200

    test(navigation): create app start integration test
    This should fail in case of an exception occurring during startup.

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,15 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:lifting_progress_tracker/main.dart' as app;
+import 'package:lifting_progress_tracker/pages/home.dart';
+import 'package:lifting_progress_tracker/pages/statistics.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('smoke tests', () {
-    testWidgets('start application', (tester) async {
+  group('navigation smoke tests', () {
+    testWidgets('start application without errors', (tester) async {
       app.main();
       await tester.pumpAndSettle();
+      expect(find.byType(HomePage), findsOneWidget);
+    });
+
+    testWidgets('navigate from home to statistics without errors',
+        (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      await tester.tap(find.widgetWithText(ElevatedButton, "Statistik"));
+      await tester.pumpAndSettle();
+      expect(find.byType(StatisticsPage), findsOneWidget);
     });
   });
 }
