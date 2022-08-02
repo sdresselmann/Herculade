65afdef68dc075df4ed2d46f325fce6b8062c966
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-08-02 18:33:59 +0200

    refactor: extract routing table to constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:firedart/firedart.dart';
 import 'package:flutter/material.dart';
 import 'package:lifting_progress_tracker/constants/routes.dart';
-import 'package:lifting_progress_tracker/pages/calendar.dart';
-import 'package:lifting_progress_tracker/pages/exercise.dart';
-import 'package:lifting_progress_tracker/pages/home.dart';
-import 'package:lifting_progress_tracker/pages/statistics.dart';
-import 'package:lifting_progress_tracker/pages/training_day.dart';
-import 'package:lifting_progress_tracker/pages/training_period.dart';
-import 'package:lifting_progress_tracker/pages/training_plan.dart';
-import 'package:lifting_progress_tracker/pages/training_plan_list.dart';
 
 /// Firebase API Key
 const apiKey = 'AIzaSyAAvQNgL2DNb7dT-7UjsJoeV8u8LJWcVhc';
@@ -36,16 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       initialRoute: homeRoute,
-      routes: {
-        homeRoute: (context) => HomePage(),
-        statisticsRoute: (context) => StatisticsPage(),
-        calendarRoute: (context) => CalendarPage(),
-        trainingplanListRoute: (context) => TrainingPlanListPage(),
-        trainingDayRoute: (context) => TrainingDayPage(),
-        exerciseRoute: (context) => ExercisePage(),
-        trainingPeriodRoute: (context) => TrainingPeriodPage(),
-        trainingPlanRoute: (context) => TrainingPlanPage(),
-      },
+      routes: routesObject,
     );
   }
 }
