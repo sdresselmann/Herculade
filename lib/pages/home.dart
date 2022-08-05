26e1eea4f20f581ea09669548e8b4c17dac3cfdc
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-08-05 12:06:12 +0200

    feat(navigation): add footer nav bar to home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,25 +14,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Calendar',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Statistic',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text(_title)),
-      body: Row(
-        children: [
-          getNavigationButton(
-            context,
-            statisticsRoute,
-          ),
-          getNavigationButton(
-            context,
-            calendarRoute,
-          ),
-          getNavigationButton(
-            context,
-            trainingplanListRoute,
-          ),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month), label: 'Kalender'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.wordpress), label: 'Statistik'),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
