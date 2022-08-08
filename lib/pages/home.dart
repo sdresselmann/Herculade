aeb3d9a2e1a43211560ff2c377c8e8b8fc171e55
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-08-08 11:33:46 +0200

    refactor: extract navigation bar items for better readability

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -34,34 +34,38 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  /// Clickable icons inside of the navigation bar. Require a [onTap] method to
+  /// be declared for the bottom app bar.
+  static const List<BottomNavigationBarItem> _navigationBarItems = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'Home',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.calendar_month),
+      label: 'Kalender',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.fitness_center),
+      label: 'Trainingspläne',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.analytics),
+      label: 'Statistik',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text(_title)),
       body: Center(child: _navigationItems.elementAt(_selectedIndex)),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month),
-            label: 'Kalender',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: 'Trainingspläne',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.analytics),
-            label: 'Statistik',
-          ),
-        ],
+        items: _navigationBarItems,
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.blueAccent[800],
         onTap: _onItemTapped,
-        // When using more than 3 items inside a bottom navigation bar the
+        // When using more than 3 items inside of a bottom navigation bar the
         // type is automatically changed to "shifting".
         //This messes with styling/formating which is not desired and requires
         //the type to be manually set.
