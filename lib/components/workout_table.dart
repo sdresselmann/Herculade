aa886f4ea2d55c744ffe10177a8cc7163c03b652
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-08-17 11:19:14 +0200

    feat(trainingplan): add removal function to button

--- lib/components/workout_table.dart
+++ lib/components/workout_table.dart
@@ -2,17 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:lifting_progress_tracker/constants/localization.dart';
 import 'package:lifting_progress_tracker/models/plan_entry.dart';
 
-class WorkoutTable extends StatelessWidget {
+const _headerStyle = TextStyle(fontWeight: FontWeight.bold);
+
+final TableRow _tableHeader = TableRow(
+  children: <TableCell>[
+    TableCell(
+      child: Text(
+        tableHeaderLabels.elementAt(0),
+        style: _headerStyle,
+      ),
+    ),
+    TableCell(
+      child: Text(
+        tableHeaderLabels.elementAt(1),
+        style: _headerStyle,
+      ),
+    ),
+    TableCell(
+      child: Text(
+        tableHeaderLabels.elementAt(2),
+        style: _headerStyle,
+      ),
+    ),
+    //Column for removal buttons.
+    const TableCell(child: Text(" "))
+  ],
+);
+
+class WorkoutTable extends StatefulWidget {
   final List<PlanEntry> tableEntries;
   const WorkoutTable({required this.tableEntries});
 
+  @override
+  State<WorkoutTable> createState() => _WorkoutTableState();
+}
+
+class _WorkoutTableState extends State<WorkoutTable> {
   @override
   Widget build(BuildContext context) {
     return Table(
       border: TableBorder.all(),
       children: <TableRow>[
         _tableHeader,
-        for (var item in tableEntries)
+        for (var item in widget.tableEntries)
           TableRow(
             children: <TableCell>[
               TableCell(
@@ -23,7 +55,11 @@ class WorkoutTable extends StatelessWidget {
               TableCell(
                 child: ElevatedButton(
                   child: const Text("-"),
-                  onPressed: () => {tableEntries.remove(item)},
+                  onPressed: () => {
+                    setState(() {
+                      widget.tableEntries.remove(item);
+                    })
+                  },
                 ),
               )
             ],
@@ -31,31 +67,4 @@ class WorkoutTable extends StatelessWidget {
       ],
     );
   }
-
-  static final TableRow _tableHeader = TableRow(
-    children: <TableCell>[
-      TableCell(
-        child: Text(
-          tableHeaderLabels.elementAt(0),
-          style: _headerStyle,
-        ),
-      ),
-      TableCell(
-        child: Text(
-          tableHeaderLabels.elementAt(1),
-          style: _headerStyle,
-        ),
-      ),
-      TableCell(
-        child: Text(
-          tableHeaderLabels.elementAt(2),
-          style: _headerStyle,
-        ),
-      ),
-      //Column for removal buttons.
-      const TableCell(child: Text(" "))
-    ],
-  );
 }
-
-const _headerStyle = TextStyle(fontWeight: FontWeight.bold);
