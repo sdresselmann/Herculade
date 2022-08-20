505693f9944c394261201909d876408958cfd8ce
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-08-20 11:25:02 +0200

    refactor: add state to table text field
    Requirement to mutate the data inside of the table.

--- lib/components/table_text_field.dart
+++ lib/components/table_text_field.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
-class TableTextField extends StatelessWidget {
+class TableTextField extends StatefulWidget {
   final String textFieldValue;
-
   const TableTextField(this.textFieldValue);
 
   @override
+  State<StatefulWidget> createState() => _TableTextFieldState();
+}
+
+class _TableTextFieldState extends State<TableTextField> {
+  @override
   Widget build(BuildContext context) {
     return EditableText(
-      controller: TextEditingController(text: textFieldValue),
+      controller: TextEditingController(text: widget.textFieldValue),
       focusNode: FocusNode(),
       style: const TextStyle(color: Colors.black),
       cursorColor: Colors.orange,
