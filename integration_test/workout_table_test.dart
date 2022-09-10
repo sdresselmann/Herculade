07d6dff241aadcac25b3ed863ba772a50dc0be8d
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-09-10 10:36:14 +0200

    tests: remove unnecessary integration test case for workout table

--- integration_test/workout_table_test.dart
+++ integration_test/workout_table_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:lifting_progress_tracker/components/workout_table.dart';
 import 'modules/navigate.dart';
-import 'modules/provider.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -16,23 +15,6 @@ void main() {
       await startApp(tester);
       await navigateToTrainingPlan(tester);
       expect(find.byType(AddEntryButton), findsOneWidget);
-    });
-  });
-  group('base functionality tests', () {
-    testWidgets(
-        'When the add entry button is tapped an entry is added to the table (expected length is equal to current length)',
-        (tester) async {
-      await startApp(tester);
-      await navigateToTrainingPlan(tester);
-
-      await setTableProviderData(tester);
-
-      int currentLength = await getTableEntryCount(tester);
-      final int expectedLength = currentLength + 1;
-      await tester.tap(find.byType(AddEntryButton));
-      await tester.pumpAndSettle();
-      currentLength = await getTableEntryCount(tester);
-      expect(currentLength == expectedLength, true);
     });
   });
 }
