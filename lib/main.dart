da7b53becf3abbc35dac8376e7bc943a38cc77e3
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-09-15 12:18:07 +0200

    refactor: add environment provider for application context
    The environment the app is called. For example: RELEASE, PRODUCTION or TEST.
    In test mode data is not fetched from firebase and has to be mocked instead.
    For backend testing use DATATEST which uses a special test database.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:lifting_progress_tracker/constants/localization.dart';
 import 'package:lifting_progress_tracker/constants/routes.dart';
+import 'package:lifting_progress_tracker/providers/environment_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(
+      environment: "PRODUCTION",
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  /// The environment the app is called. For example: RELEASE, PRODUCTION or TEST.
+  ///
+  /// In test mode data is not fetched from firebase and has to be mocked instead.
+  /// For backend testing use DATATEST which uses a special test database.
+  final String environment;
+  const MyApp({Key? key, required this.environment}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Herculade',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Provider(
+      create: (context) => EnvironmentProvider(environment: environment),
+      child: MaterialApp(
+        title: 'Herculade',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: homeRouteLabel,
+        routes: routesObject,
       ),
-      initialRoute: homeRouteLabel,
-      routes: routesObject,
     );
   }
 }
--- lib/pages/training_plan.dart
+++ lib/pages/training_plan.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:lifting_progress_tracker/components/navigation_button.dart';
 import 'package:lifting_progress_tracker/components/workout_table.dart';
 import 'package:lifting_progress_tracker/constants/localization.dart';
+import 'package:lifting_progress_tracker/providers/environment_provider.dart';
 import 'package:lifting_progress_tracker/providers/table_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -20,6 +20,10 @@ class _TrainingPlanPageState extends State<TrainingPlanPage> {
 
   @override
   Widget build(BuildContext context) {
+    final bool environmentIsProd =
+        Provider.of<EnvironmentProvider>(context, listen: false).environment ==
+            'PRODUCTION';
+
     return Scaffold(
       appBar: AppBar(title: const Text(_title)),
       body: Column(
@@ -29,7 +33,7 @@ class _TrainingPlanPageState extends State<TrainingPlanPage> {
               ChangeNotifierProvider(
                 create: (context) => TableProvider(
                   trainingPlanId: "trainingplan1",
-                  fetchEntries: true,
+                  fetchEntries: environmentIsProd,
                 ),
               )
             ],
