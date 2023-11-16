f5048a5e37bd6cc00b408b28d40a1757ad9a0692
sdresselmann <46092447+sdresselmann@users.noreply.github.com>
2023-11-16 13:07:19 +0100

    refactor(firebase): use getIt for service
    This allows for more modularity and flexibility, due to using dependency injection. This should make mocking for tests much cleaner.

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:lifting_progress_tracker/firebase_options.dart';
-import 'package:lifting_progress_tracker/services/auth_service.dart';
 import 'package:logging/logging.dart';
 
 class FirebaseService {
@@ -12,13 +11,5 @@ class FirebaseService {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     ).then((value) => log.info("Firebase App initialized"));
-
-    // for now just use a test user
-    authenticateUser();
-  }
-
-  void authenticateUser() {
-    final AuthService authService = AuthService();
-    authService.signInTestUser();
   }
 }
