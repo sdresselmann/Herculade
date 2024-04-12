63d01d75bc2a6fad5c2f5d32c54921928ed9d695
sdresselmann <46092447+sdresselmann@users.noreply.github.com>
2024-04-12 11:21:53 +0200

    #60 - feat(user specific views): authenticate user in service
    Currently only a fixed TestUser is used for authentication.

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,13 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lifting_progress_tracker/firebase/services/firebase_auth_service.dart';
 import 'package:logging/logging.dart';
 
 class AuthService {
-  final Logger log;
+  final Logger _logger;
+  User? _user;
 
-  AuthService() : log = Logger('AuthService');
+  AuthService() : _logger = Logger('AuthService');
 
   void authenticateUser() {
     final FirebaseAuthService authService = FirebaseAuthService();
-    authService.signInTestUser();
+    authService.signInTestUser()
+        .then((User user) => {
+          _user = user,
+          _logger.log(Level.INFO, '${user.email} successfully authenticated.'),
+    },);
   }
+
+  User? getAuthenticatedUser() {
+    return _user;
+  }
+
 }
--- lib/firebase/services/firebase_auth_service.dart
+++ lib/firebase/services/firebase_auth_service.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-// ignore_for_file: uri_does_not_exist
 import 'package:lifting_progress_tracker/config/auth_config.dart';
 import 'package:logging/logging.dart';
 
@@ -7,21 +6,20 @@ class FirebaseAuthService {
   final Logger log;
   FirebaseAuthService() : log = Logger('FirebaseAuthService');
 
-  Future<void> signInTestUser() async {
+  Future<User> signInTestUser() async {
     final FirebaseAuth auth = FirebaseAuth.instance;
 
     try {
       final UserCredential userCredential =
           await auth.signInWithEmailAndPassword(
-        // ignore: undefined_identifier
-        email: AuthConfig.testUserEmail,
-
-        // ignore: undefined_identifier
-        password: AuthConfig.testUserPassword,
+            email: AuthConfig.testUserEmail,
+            password: AuthConfig.testUserPassword,
       );
       log.info("Signed in: ${userCredential.user!.email}");
+      return Future.value(userCredential.user);
     } catch (e) {
       log.severe("Error signing in: $e");
+      throw Exception();
     }
   }
 }
